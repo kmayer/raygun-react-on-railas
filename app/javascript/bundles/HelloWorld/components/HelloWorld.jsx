c8d949f7c23a697cbedd8f9f0e85fb7fa8bc09f9
Ken Mayer <ken.mayer@carbonfive.com>
2019-10-17 14:09:35 -0700

    add redux-starter-kit

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,25 +1,41 @@
-import PropTypes from "prop-types";
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { updateName } from "../store/helloWordSlice";
 
-const HelloWorld = ({ name, updateName }) => (
-  <div>
-    <h3>Hello, {name}!</h3>
-    <hr />
-    <form>
-      <label htmlFor="name">Say hello to:</label>
-      <input
-        id="name"
-        type="text"
-        value={name}
-        onChange={e => updateName(e.target.value)}
-      />
-    </form>
-  </div>
-);
+const mapDispatch = { updateName };
 
-HelloWorld.propTypes = {
+// Which part of the Redux global state does our component want to receive as props?
+const mapStateToProps = state => ({ name: state.name });
+
+const HelloWorldComponent = ({ name, updateName }) => {
+  return (
+    <div>
+      <h3>Hello, {name}!</h3>
+      <hr />
+      <form>
+        <label htmlFor="name">Say hello to:</label>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={e => updateName(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+};
+
+HelloWorldComponent.propTypes = {
   name: PropTypes.string.isRequired,
   updateName: PropTypes.func.isRequired
 };
 
-export default HelloWorld;
+export const { HelloWorld } = HelloWorldComponent;
+
+// Note that we don't use HelloWorldComponent, but the redux "connected" version of it.
+// See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
+export default connect(
+  mapStateToProps,
+  mapDispatch
+)(HelloWorldComponent);
--- app/javascript/bundles/HelloWorld/reducers/helloWorldReducer.test.jsx
+++
@@ -1,8 +0,0 @@
-import updateName from "./helloWorldReducer";
-import { HELLO_WORLD_NAME_UPDATE } from "../constants/helloWorldConstants";
-
-test("responds to the action", () => {
-  const action = { type: HELLO_WORLD_NAME_UPDATE, text: "Hello, world." };
-
-  expect(updateName("", action)).toBe("Hello, world.");
-});
